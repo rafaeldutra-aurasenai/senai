e0689a191bd94f6731c8f8451f4ef066b35bb643
rafaeldutra-aurasenai <rafael.h.dutra@aluno.senai.br>
2026-02-25 15:50:52 -0300

    aula do dia 25/02/2026 aprendendo mais sobre scripts

--- front-end/aula_4/src/script/aula4.js
+++ front-end/aula_4/src/script/aula4.js
@@ -1,14 +1,60 @@
-const tempoAgora = new Date()
+// const tempoAgora = new Date()
 
 
-const hora = tempoAgora.getHours()
+// const hora = tempoAgora.getHours()
 
 
-const dataFormatada = tempoAgora.toLocaleDateString('pt-BR')
-const horaFormatada = tempoAgora.toLocaleTimeString('pt-BR')
+// const dataFormatada = tempoAgora.toLocaleDateString('pt-BR')
+// const horaFormatada = tempoAgora.toLocaleTimeString('pt-BR')
 
 
-console.log("hoje é dia: " + dataFormatada)
-console.log("Horario:" + horaFormatada)
+// console.log("hoje é dia: " + dataFormatada)
+// console.log("Horario:" + horaFormatada)
 
-console.log("só a hora:" + hora)
+// console.log("só a hora:" + hora)
+
+// if(hora>12 && hora<18){
+//     alert("Boa tarde!");
+// } else if(hora >=18 && hora <6){
+//     alert("Boa noite")
+// }
+
+// alert("Seja bem-vindo ao TechFood! Aproveite nossos cupons de hoje.");~
+
+
+class Prato{
+    constructor(nome, preco){
+        this.nome = nome
+        this.preco = preco
+    }
+
+    exibirComMoeda(resultado){
+        if(resultado){
+            return "R$" + resultado.toFixed(2);
+        }else{
+            return "R$" + this.preco.toFixed(2);
+        }
+    }
+
+//     exibirComMoeda(total){
+//         return "R$" + this.total.toFixed(2)
+//     }
+ }
+ alert("bem vindo ao restaurante sabor e saber ")
+
+const cliente = prompt("para um atendimento personalizado. digite seu nome: ")
+
+let clienteFormatado = cliente.trim().toUpperCase()
+
+alert("bem vindo " +   clienteFormatado +   " ao restaurante sabor e saber")
+
+const lasanha = new Prato("Lasanha Bolonhesa", 45.00)
+
+let qtd = prompt("quantas unidades de: "  +  lasanha.nome + " você deseja?")
+
+let total = lasanha.preco * qtd
+
+alert ("Resumo da simulacao: \n Prato"+ lasanha.nome + "\nTotal:" + lasanha.exibirComMoeda(total))
+
+alert("bem vindo ao restaurante sabor e saber ")
+
---
+++ front-end/aula_4/src/script/exerciciosAula4.js
@@ -0,0 +1,26 @@
+//exercicio 1
+// const cliente = prompt("digite seu nome: ")
+// const cliente1 = prompt("digite seu sobrenome: ")
+// let clienteFormatado = cliente.trim()
+// let clienteFormatado1 = cliente1.trim()
+// alert("bem vindo " + clienteFormatado + " "+ clienteFormatado1)
+// let contagem = cliente.length + cliente1.length
+
+// alert("seu nome tem "+ contagem + "letras")
+
+//exercicio 2
+// const conta = prompt("digite o valor total da conta: ")
+// const numeroPessoas = prompt("digite o valor de pessoas: ")
+
+// let valor = conta / numeroPessoas
+
+// alert("Cada amigo deve pagar R$ " + valor.toFixed(2))
+
+//exercicio 3 
+// const valor = prompt("digite o valor total da compra: ")
+// const cupom = prompt("possui cupom sim ou nao?")
+// if(valor>=150 ||cupom.toLowerCase() === "sim" ){
+//     alert("tudo certo,frete liberado")
+// }else{
+//     alert("nao ganhou frete")
+// }
