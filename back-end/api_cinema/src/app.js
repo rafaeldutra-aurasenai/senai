b834de0f8217a8220b0249fba755a1025a0e9aaa
rafaeldutra-aurasenai <rafael.h.dutra@aluno.senai.br>
2026-03-04 12:11:41 -0300

    aula back-end dia 04/03/2026 catch

--- back-end/api_cinema/src/app.js
+++ back-end/api_cinema/src/app.js
@@ -3,16 +3,77 @@ const pool = require('./config/database')
 
 const app = express()
 
-app.use(express.json)
-
-app.get('/',(req, res)=>{
-    res.send('API cinema esta funcionando!!')
-})
-
-app.get('/filmes',(req,res)=>{
-    pool.query('SELECT * FROM filme', (err, results) => {
-        res.json(results)
+app.use(express.json())
+const queryAsync =(sql,values =[]) =>{
+    return new Promise((resolve,reject)=>{
+        pool.query (sql,values, (err,results)=>{
+            if(err) reject(err)
+            else resolve(results)
+        })
     })
+}
+app.get('/', (req,res) => {
+    res.send("API CINEMA")
 })
+
+app.get('/filmes', async (req, res) => {
+    try{
+        const filmes = await queryAsync('SELECT * FROM filme')
+        res.json({
+            sucesso:true,
+            dados:filmes,
+            total:filmes.length
+        })
+    }catch(erro){
+        console.error('Erro ao listar filmes:', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem:'Erro ao listar filmes',
+            erro: erro.message
+        })
+    }
+})
+
+// app.get('/filmes', (req,res) => {
+//     pool.query('SELECT * FROM filme', (err, results) =>{
+//         res.json(results)
+//     })
+// })
+app.get('/filmes/:id',async (req,res)=>{
+    try{
+        const{id} = req.params
+        if(!id || isNaN(id)){
+            return res.status(400).json({
+                sucesso:false,
+                mensagem:'o id de filme invalido'
+            })
+        }
+        const filme= await queryAsync('SELECT * FROM filme WHERE id = ?', {id})
+        if(filme.length === 0){
+            return res.status(404).json({
+                sucesso:false,
+                mensagem:'filme nao encontrado'
+            })
+        }
+        res.json({
+            sucesso:true,
+            dados:filmes[0]
+        })
+    }catch(erro){
+        console.error('Erro ao listar filmes:', erro)
+        res.status(500).json({
+            sucesso: false,
+            mensagem:'Erro ao listar filmes',
+            erro: erro.message
+        })
+})
+
+// app.get('/filmes/:id', (req,res) => {
+//     const {id} = req.params
+
+//     pool.query('SELECT * FROM filme WHERE id = ?', [id],(err, results) =>{
+//         res.json(results)
+//     })
+//})
 
 module.exports = app
--- back-end/api_cinema/src/server.js
+++ back-end/api_cinema/src/server.js
@@ -1,19 +1,18 @@
-const pool = require('./config/database')
-const app = require('./app')
+const app = require('./app');
+const pool = require('./config/database');
 
-const PORT= 3000;
+const PORT = 3000;
 
-pool.getConnection((err,connection)=>{
-    if(err){
-        console.log('erro ao conectar ao banco:',err)
-        process.exit(1)
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Erro ao conectar no banco:', err);
+        process.exit(1);
     }
 
-    console.log('conectado ao MySQL!!')
-    connection.release()
+    console.log('Conectado ao MySQL com sucesso! 🎉');
+    connection.release();
+});
 
-})
-
-app.listen(PORT,()=>{
-    console.log('servidor rodando!!')
-})
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
