369f54803c96974cba22e8ba97a28f3c24422999
rafaeldutra-aurasenai <rafael.h.dutra@aluno.senai.br>
2026-03-25 15:53:50 -0300

    aula dia 25/03/26 marlon

--- front-end/exercicios_formativa/src/script/script_aula6.js
+++ front-end/exercicios_formativa/src/script/script_aula6.js
@@ -1,1 +1,8 @@
-const btnCurtir
+const btncurtir = document.querySelectorAll("#btn-curtir");
+const contador = document.getElementById("contador");
+
+
+    btncurtir.addEventListener("click", () => {
+        contador++;
+        span.textContent = contador;
+    });
