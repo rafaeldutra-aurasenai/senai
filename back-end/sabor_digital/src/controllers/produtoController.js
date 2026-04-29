91e6165612ee7e81753da8f94bb111fe2b0dbc65
rafaeldutra-aurasenai <rafael.h.dutra@aluno.senai.br>
2026-04-29 16:00:05 -0300

    ultimas atualizacoes do dia 29

--- back-end/sabor_digital/src/controllers/produtoController.js
+++ back-end/sabor_digital/src/controllers/produtoController.js
@@ -17,9 +17,14 @@ class ProdutoController{
     async buscarPorId(req, res){
         try {
             const resultado = await ProdutoService.buscarProdutoPorId(req.params.id)
+            res.json(resultado)
             
         } catch (error) {
-            
+            res.status(500).json({
+            sucesso: false,
+            mensagem:error.mensagem || 'Erro interno do servidor',
+            erro:erro
+        })
         }
     }
 }
